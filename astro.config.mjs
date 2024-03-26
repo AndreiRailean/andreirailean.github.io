4843039ca6cd2da058d7e1ab8a016f068c810a9b
Andrei Railean <andrei@siter.com.au>
2024-03-27 07:01:00 +1100

    first version is looking good now

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://andrei.md",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    icon({
+      include: {
+        "fa6-brands": ["*"]
+      },
+    })
+  ],
+  output: "static",
+  server: {
+    port: 4354
+  }
+});
