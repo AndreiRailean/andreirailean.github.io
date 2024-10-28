b3211a0e3637d32e098726c3487c50071ff3fbe8
Andrei Railean <andrei@siter.com.au>
2024-10-28 14:14:28 +1100

    increase avatar size

--- src/components/MyAvatar.tsx
+++ src/components/MyAvatar.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export const MyAvatar = () => (
-  <Avatar className="h-24 w-24 border-4 border-gray-300">
+  <Avatar className="h-32 w-32 border-4 border-gray-300">
     <AvatarImage
       src="https://avatars.githubusercontent.com/u/25991?v=4"
       alt="Andrei Railean"
