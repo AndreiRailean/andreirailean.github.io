13840dd2913d9ff98b118be27a77abe133268a20
Andrei Railean <andrei@siter.com.au>
2025-01-07 13:36:36 +1100

    upgrade eslint and other deps

---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import globals from "globals"
+import tsParser from "@typescript-eslint/parser"
+import astroEslintParser from "astro-eslint-parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintPluginAstro from "eslint-plugin-astro"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**/*"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.ts"],
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+
+    languageOptions: {
+      parser: astroEslintParser,
+      ecmaVersion: 5,
+      sourceType: "script",
+
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+  {
+    files: ["**/*.astro/*.js", "*.astro/*.js"],
+
+    languageOptions: {
+      parser: tsParser,
+    },
+  },
+)
