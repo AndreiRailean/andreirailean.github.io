8b79b783321a9bac98f903c43bf3fadb655fc0f0
Andrei Railean <andrei@siter.com.au>
2025-02-05 06:26:50 +1100

    upgrade to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
 
 import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss({
+        applyBaseStyles: false,
+      }),
+    ],
+  },
   site: "https://andrei.md",
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     react(),
     icon({
       include: {
